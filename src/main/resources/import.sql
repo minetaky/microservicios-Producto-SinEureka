dcbabac82bd3ff0c6b6ab083a2b13fdee4453337
Veronica M. Melendez Montiel <minetaky@gmail.com>
2022-09-29 12:02:17 -0500

    Proyecto de microservicios sin Eureka

---
+++ src/main/resources/import.sql
@@ -0,0 +1,9 @@
+INSERT INTO productos(nombre, precio, create_at) VALUES ('Panasonic', 800, NOW());
+INSERT INTO productos(nombre, precio, create_at) VALUES ('Sonic', 700, NOW());
+INSERT INTO productos(nombre, precio, create_at) VALUES ('Apple', 800, NOW());
+INSERT INTO productos(nombre, precio, create_at) VALUES ('Sony NoteBook', 1000, NOW());
+INSERT INTO productos(nombre, precio, create_at) VALUES ('Hewlett Packard', 500, NOW());
+INSERT INTO productos(nombre, precio, create_at) VALUES ('Bianchi', 600, NOW());
+INSERT INTO productos(nombre, precio, create_at) VALUES ('Nike', 100, NOW());
+INSERT INTO productos(nombre, precio, create_at) VALUES ('Addidas', 200, NOW());
+INSERT INTO productos(nombre, precio, create_at) VALUES ('Reebok', 300, NOW());
